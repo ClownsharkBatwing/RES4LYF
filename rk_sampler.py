e279d0a945efee26a69290dae081a13ede3c5a75
ClownsharkBatwing <clownshark.batwing@proton.me>
2025-01-04 05:41:05 -0500

    Add files via upload

--- rk_sampler.py
+++ rk_sampler.py
@@ -14,7 +14,7 @@ from .noise_sigmas_timesteps_scaling import get_res4lyf_step_with_model, get_res
 from .rk_method import RK_Method
 from .rk_guide_func import *
 
-from .latents import normalize_latent, initialize_or_scale
+from .latents import normalize_latent, initialize_or_scale, latent_normalize_channels
 from .helper import get_extra_options_kv, extra_options_flag
 from .sigmas import get_sigmas
 
@@ -271,7 +271,12 @@ def sample_rk(model, x, sigmas, extra_args=None, callback=None, disable=None, no
                     eps_[row] *= 1 + substep_noise_scaling*(substep_noise_scaling_ratio-1)
 
                 cossim_cutoff = float(get_extra_options_kv("epsilon_cossim_cutoff", "1.0", extra_options))
-                if cossim_cutoff > get_cosine_similarity(data_[0], y0):
+                #data_norm = latent_normalize_channels(data_[0]) #torch.norm(data_[0], dim=-3)
+                #y0_norm = latent_normalize_channels(y0) #torch.norm(y0, dim=-3)
+                data_norm = data_[0] - data_[0].mean(dim=(2, 3), keepdim=True)
+                y0_norm = y0 - y0.mean(dim=(2, 3), keepdim=True)
+                print(get_cosine_similarity(data_norm, y0_norm))
+                if cossim_cutoff > get_cosine_similarity(data_norm, y0_norm):
                     eps_, x_ = process_guides_substep(x_0, x_, eps_, data_, row, y0, y0_inv, lgw[step], lgw_inv[step], lgw_mask, lgw_mask_inv, step, sigma, sigma_next, sigma_down, s_, unsample_resample_scale, rk, rk_type, guide_mode, latent_guide_inv, UNSAMPLE, extra_options)
             
             x = x_0 + h * rk.b_k_sum(eps_, 0)
@@ -381,7 +386,7 @@ def sample_rk(model, x, sigmas, extra_args=None, callback=None, disable=None, no
                 x = rk.add_noise_post(x, sigma_up, sigma, sigma_next, alpha_ratio, s_noise, noise_mode, SDE_NOISE_EXTERNAL, sde_noise_t)
 
 
-        #print("Data vs. y0 cossim score: ", get_cosine_similarity(data_[0], y0))
+        print("Data vs. y0 cossim score: ", get_cosine_similarity(data_[0], y0))
 
         for ms in range(rk.multistep_stages):
             eps_ [rk.multistep_stages - ms] = eps_ [rk.multistep_stages - ms - 1]
@@ -389,7 +394,7 @@ def sample_rk(model, x, sigmas, extra_args=None, callback=None, disable=None, no
         eps_ [0] = torch.zeros_like(eps_ [0])
         data_[0] = torch.zeros_like(data_[0])
         
-        #print("Denoised vs. y0 cossim score: ", get_cosine_similarity(denoised, y0))
+        print("Denoised vs. y0 cossim score: ", get_cosine_similarity(denoised, y0))
         denoised_prev = denoised
         eps_prev = eps
         
