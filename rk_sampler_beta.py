ae42c00f41fe1a36e9a52bd65dad76225fa00cd6
ClownsharkBatwing <clownshark.batwing@proton.me>
2025-01-21 12:05:33 -0500

    Add files via upload

--- rk_sampler_beta.py
+++ rk_sampler_beta.py
@@ -179,6 +179,10 @@ def sample_rk_beta(model, x, sigmas, extra_args=None, callback=None, disable=Non
                             pass
                         elif diag_iter > 0 and extra_options_flag("implicit_substep_only_first_eta", extra_options):
                             pass
+                        elif full_iter > 0 and full_iter == implicit_steps_full and extra_options_flag("implicit_step_skip_final_eta", extra_options):
+                            pass
+                        elif full_iter > 0 and extra_options_flag("implicit_step_only_first_eta", extra_options):
+                            pass
                         elif row < rk.rows-rk.multistep_stages   or   diag_iter < implicit_steps_diag:
                             sub_sigma_up, sub_sigma, sub_sigma_down, sub_alpha_ratio = get_res4lyf_step_with_model(model, s_[row], s_[row+row_offset+rk.multistep_stages], eta_substep, eta_var_substep, noise_mode_sde_substep)
                             h_new = h * rk.h_fn(sub_sigma_down, sigma) / rk.h_fn(sub_sigma_next, sigma) 
