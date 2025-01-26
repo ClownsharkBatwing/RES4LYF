78c87daac24cd4e7187089f588dc5393d6287cc3
ClownsharkBatwing <clownshark.batwing@proton.me>
2025-01-25 22:36:56 -0500

    Add files via upload

--- rk_sampler_beta.py
+++ rk_sampler_beta.py
@@ -294,11 +294,16 @@ def sample_rk_beta(model, x, sigmas, extra_args=None, callback=None, disable=Non
                         x_[r] = x_0 + h * (rk.b_k_sum(eps_, 0) + rk.v_k_sum(eps_prev_, 0))
                     if sigma == s_[r]:
                         continue
+                    #x_[r] = x_[r] + newton_iter_mixing_rate * (x_tmp - x_[r])
                     data_[r] = get_data_from_step(x_0, x_[r], sigma, s_[r])  
                     eps_ [r] = get_epsilon(x_0, data_[r], s_[r], rk_type)
                     
-                    x_[r] = x_[r] + newton_iter_mixing_rate * (x_tmp - x_[r])
-                    eps_[r] = eps_[r] + newton_iter_mixing_rate * (eps_tmp - eps_[r])
+                    #for r2 in range(0, rk.rows+1): 
+                    #    if r != r2:
+                    #        eps_ [r] = get_orthogonal(eps_[r2], eps_[r])
+                    
+                    x_[r] = x_tmp + newton_iter_mixing_rate * (x_[r] - x_tmp)
+                    eps_[r] = eps_tmp + newton_iter_mixing_rate * (eps_[r] - eps_tmp)
                     
             newton_iter_alt_init = int(get_extra_options_kv("newton_iter_alt_init", str("0"), extra_options))
             if step == len(sigmas)-4:
@@ -317,8 +322,8 @@ def sample_rk_beta(model, x, sigmas, extra_args=None, callback=None, disable=Non
 
 
         if extra_options_flag("guide_fully_pseudoimplicit_power_substep", extra_options) or extra_options_flag("inject_fully_pseudoimplicit_power_substep", extra_options) or extra_options_flag("guide_relaxing_fully_pseudoimplicit_power_substep", extra_options):
-            inj_proj_weight = inject_fully_pseudoimplicit_power_substep_projection = float(get_extra_options_kv("inject_fully_pseudoimplicit_power_substep_projection", "0.0", extra_options))
-            inj_weight = inject_fully_pseudoimplicit_power_substep = float(get_extra_options_kv("inject_fully_pseudoimplicit_power_substep", "0.0", extra_options))
+            inj_proj_weight = inject_fully_pseudoimplicit_power_substep_projection = float(get_extra_options_kv("inject_fully_pseudoimplicit_power_substep_projection", "1.0", extra_options))
+            inj_weight = inject_fully_pseudoimplicit_power_substep = float(get_extra_options_kv("inject_fully_pseudoimplicit_power_substep", "1.0", extra_options))
             inj_w = max(inj_weight, inj_proj_weight)
             
             x_lying_ = x_.clone()
@@ -347,7 +352,7 @@ def sample_rk_beta(model, x, sigmas, extra_args=None, callback=None, disable=Non
                         x_[r] = x_0 + h * (rk.b_k_sum(eps_, 0) + rk.v_k_sum(eps_prev_, 0))
                 x_lying_[r] = x_[r] + rk.h_fn(fully_sub_sigma_2, s_[r]) * eps_substep_guide
                 data_tmp = get_data_from_step(x_0, x_lying_[r], sigma, s_lying_[r])
-                if rk.h_fn(s_lying_[r], sigma).abs() > h.abs() / 100: 
+                if rk.h_fn(s_lying_[r], sigma).abs() > h.abs() / 100:           # if this ratio is too great, bad results
                     eps_lying_[r] = get_epsilon(x_0, data_tmp, sigma, rk_type)
                 
             eps_ = eps_lying_
@@ -370,11 +375,18 @@ def sample_rk_beta(model, x, sigmas, extra_args=None, callback=None, disable=Non
                         x_[r] = x_0 + h * (rk.b_k_sum(eps_, 0) + rk.v_k_sum(eps_prev_, 0))
                     if sigma == s_[r]:
                         continue
+                    #x_[r] = x_[r] + newton_iter_mixing_rate * (x_tmp - x_[r])
                     data_[r] = get_data_from_step(x_0, x_[r], sigma, s_[r])
                     eps_ [r] = get_epsilon(x_0, data_[r], s_[r], rk_type)
                     
-                    x_[r] = x_[r] + newton_iter_mixing_rate * (x_tmp - x_[r])
-                    eps_[r] = eps_[r] + newton_iter_mixing_rate * (eps_tmp - eps_[r])
+                    for r2 in range(0, rk.rows+1): 
+                        if r != r2:
+                            #eps_ [r2] = get_orthogonal(eps_[r2], eps_[r])
+                            eps_ [r] = get_orthogonal(eps_[r2], eps_[r])
+                    #eps_ [r] = get_collinear(eps_[r], eps_0_lying)
+                    
+                    x_[r] = x_tmp + newton_iter_mixing_rate * (x_[r] - x_tmp)
+                    eps_[r] = eps_tmp + newton_iter_mixing_rate * (eps_[r] - eps_tmp)
 
 
 
