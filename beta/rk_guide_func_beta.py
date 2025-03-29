76fd98c8ee900a5d3e7dcdfe45b1a90ebde74f8d
ozbayb <17261091+ozbayb@users.noreply.github.com>
2025-03-28 20:16:04 -0600

    lemon squeezy

--- beta/rk_guide_func_beta.py
+++ beta/rk_guide_func_beta.py
@@ -260,7 +260,7 @@ class LatentGuide:
             self.y0_inv = noise_sampler(sigma=self.sigma_max, sigma_next=self.sigma_min).to(dtype=self.dtype, device=self.device)
             self.y0_inv = normalize_zscore(self.y0_inv, channelwise=True, inplace=True)
             
-        if self.VIDEO and self.frame_weights_mgr is not None and x.shape[2] > 1:
+        if self.VIDEO and self.frame_weights_mgr is not None:
             num_frames = x.shape[2]
             self.frame_weights     = self.frame_weights_mgr.get_frame_weights_by_name('frame_weights', num_frames)
             self.frame_weights_inv = self.frame_weights_mgr.get_frame_weights_by_name('frame_weights_inv', num_frames)
@@ -300,12 +300,14 @@ class LatentGuide:
     def get_masks_for_step(self, step:int) -> Tuple[Tensor, Tensor]:
         lgw_mask, lgw_mask_inv = self.prepare_weighted_masks(step)
 
-        if self.VIDEO and self.frame_weights_mgr is not None:
+        if self.VIDEO and self.frame_weights_mgr:
             num_frames = lgw_mask.shape[2]
-            frame_weights = self.frame_weights_mgr.get_frame_weights_by_name('frame_weights', num_frames, step)
-            frame_weights_inv = self.frame_weights_mgr.get_frame_weights_by_name('frame_weights_inv', num_frames, step)
-            apply_frame_weights(lgw_mask, frame_weights)
-            apply_frame_weights(lgw_mask_inv, frame_weights_inv)
+            if self.HAS_LATENT_GUIDE:
+                frame_weights = self.frame_weights_mgr.get_frame_weights_by_name('frame_weights', num_frames, step)
+                apply_frame_weights(lgw_mask, frame_weights)
+            if self.HAS_LATENT_GUIDE_INV:
+                frame_weights_inv = self.frame_weights_mgr.get_frame_weights_by_name('frame_weights_inv', num_frames, step)
+                apply_frame_weights(lgw_mask_inv, frame_weights_inv)
 
         return lgw_mask.to(self.device), lgw_mask_inv.to(self.device)
 
@@ -371,7 +373,7 @@ class LatentGuide:
                                             full_iter                   : int,
                                             BONGMATH                    : bool,
                                             ):
-        
+
         if "pseudoimplicit" not in self.guide_mode or (self.lgw[step] == 0 and self.lgw_inv[step] == 0):
             return x_0, x_, eps_, None, None
         
@@ -634,6 +636,8 @@ class LatentGuide:
                                 sigma_row     : Tensor,
                                 frame_targets : Optional[Tensor] = None,
                                 ):
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return x_row
 
         y0, y0_inv, lgw_mask, lgw_mask_inv = self.get_cossim_adjusted_lgw_masks(data_row, step)
         
@@ -682,6 +686,9 @@ class LatentGuide:
                         sigma_row     : Tensor,
                         frame_target  : float = 1.0,
                         ):
+
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return x_row
 
         if self.guide_mode in {"data", "data_projection"}:
             data_targets = self.EO("data_targets", [1.0])
@@ -752,6 +759,9 @@ class LatentGuide:
                                 frame_targets : Optional[Tensor] = None,
                                 RK=None,
                                 ):
+        
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return eps_row
 
         y0, y0_inv, lgw_mask, lgw_mask_inv = self.get_cossim_adjusted_lgw_masks(data_row, step)
         
@@ -806,6 +816,9 @@ class LatentGuide:
                         sigma_row     : Tensor,
                         frame_target  : float = 1.0,
                         ):
+        
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return eps_row
 
         if self.guide_mode in {"epsilon", "epsilon_projection"}:
             eps_targets = self.EO("eps_targets", [1.0])
@@ -878,6 +891,9 @@ class LatentGuide:
                                 epsilon_scale :  float,
                                 RK,
                                 ):
+        
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return eps_, x_
 
         y0, y0_inv, lgw_mask, lgw_mask_inv = self.get_cossim_adjusted_lgw_masks(data_[row], step)
         
@@ -989,6 +1005,9 @@ class LatentGuide:
                                 epsilon_scale :  float,
                                 RK,
                                 ):
+        
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return eps_, x_
 
         y0, y0_inv, lgw_mask, lgw_mask_inv = self.get_cossim_adjusted_lgw_masks(data_[row], step)
         
@@ -1133,7 +1152,7 @@ class LatentGuide:
         if self.EO("substep_eps_std"):
             eps_[row] = normalize_latent(eps_[row], eps_orig[row], mean=False, channelwise=False)
         return eps_, x_
-
+    
 
     def process_channelwise(self,
                             x_0                   : Tensor,
@@ -1210,14 +1229,17 @@ class LatentGuide:
 
     @torch.no_grad
     def process_guides_poststep(self, x:Tensor, denoised:Tensor, eps:Tensor, data:Tensor, sigma_curr:Tensor, step:int) -> Tuple[Tensor, Tensor, Tensor]:
-        x_orig = x.clone()
-        mean_weight = self.EO("mean_weight", 0.01)
+        if not self.HAS_LATENT_GUIDE and not self.HAS_LATENT_GUIDE_INV:
+            return x
 
         y0, y0_inv, lgw_mask, lgw_mask_inv = self.get_cossim_adjusted_lgw_masks(denoised, step)
         
         if not (lgw_mask.any() != 0 or lgw_mask_inv.any() != 0):  # cossim score too similar! deactivate guide for this step
             return x
         
+        x_orig = x.clone()
+        mean_weight = self.EO("mean_weight", 0.01)
+
         mask = self.mask #needed for bitwise mask below
         
         if self.guide_mode in {"epsilon_dynamic_mean_std", "epsilon_dynamic_mean", "epsilon_dynamic_std", "epsilon_dynamic_mean_from_bkg"}:
